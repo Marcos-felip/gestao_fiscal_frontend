813eff5d4fe9616e36d14ab26d7874a317ce2a0b
marcos-felipe <marcosfelip2002@gmail.com>
2026-05-16 16:49:33 -0300

    Fix: remover @layer base do theme.css para que variáveis CSS sejam globais

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import router from './router'
 import { useAuthStore } from './modules/auth/presentation/stores/auth-store'
 import './style.css'
+// Importar Preline CSS na ordem correta: theme customizado ANTES do tema do Preline
 import '../node_modules/preline/css/themes/theme.css'
 
 const app = createApp(App)
