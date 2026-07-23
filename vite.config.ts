55f7e7229574986224bf2de020835df479d0c6b7
marcos-felipe <marcosfelip2002@gmail.com>
2026-07-23 13:14:34 -0300

    chore: update app assets and Vite setup
    Commitar os arquivos de infraestrutura, dependências e ícones.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import VueInspector from 'vite-plugin-vue-inspector'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    VueInspector({
+      launchEditor: 'code',
+    }),
+    vue(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': '/src',
